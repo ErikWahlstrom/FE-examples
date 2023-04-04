cfa4b5c39259aaf03ea0a489da7bb4b607a74d40
Erik Wahlström <ew@kth.se>
2023-04-04 10:12:49 +0200

    Fixed a bit with the playwright test

--- Sveltekit-dashboard/tests/test.ts
+++ Sveltekit-dashboard/tests/test.ts
@@ -2,5 +2,6 @@ import { expect, test } from '@playwright/test';
 
 test('about page has expected h1', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'About this app' })).toBeVisible();
+	await page.getByText('Dimension2 6.7 Max 4.3 Min 5.7 Average').click();
+	await page.getByRole('button').nth(1).click();
 });
